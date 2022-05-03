96f139325d61f4deaf86ab58995c8b5c586049ca
OKJI <dmitry.okladnoy@proarea.com>
2022-05-03 14:26:43 +0300

    Add Query

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+
+import 'graphql/schema.graphql.dart';
+
+final productsQuery = """
+{
+  products(first: 5, channel: "default-channel") {
+    edges {
+      node {
+        id
+        name
+        description
+      }
+    }
+  }
+}
+""";
 
 void main() async {
   final HttpLink httpLink = HttpLink(
@@ -25,8 +43,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+      home: Scaffold(body: _body()),
+    );
+  }
+
+  Widget _body() {
+    return Center(
+      child: Query(
+          options: QueryOptions(document: gql(productsQuery)),
+          builder: (result, {fetchMore, refetch}) {
+            if (result.hasException) {
+              return const Text("ERROR");
+            }
+            if (result.isLoading) {
+              return const CircularProgressIndicator();
+            }
+            log(result.data.toString());
+            // result.parsedData;
+
+            return const Text("qwe");
+          }),
     );
   }
 }
